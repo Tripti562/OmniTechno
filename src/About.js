a0adc9962ca81c1f97ea061e0360b83265874482
TriptiGautam <gautamtripti1@gmail.com>
2024-05-06 21:32:32 +0545

    about company section

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section className="my-5">
-      <div className="container d-flex">
+      <div className="container d-flex flex-wrap justify-content-evenly">
         <div className="">
           <p className="fs-5 fw-bold mainbluecolor">
             ABOUT OUR{" "}
@@ -21,7 +21,6 @@ export default function About() {
           </p>
           <hr></hr>
           <div className="d-flex flex-wrap justify-content-between">
-            
             <div>
               <div
                 class="d-flex flex-wrap  my-3 align-items-center btn btn-primary rounded-0 fw-medium border border-0 mainblue"
@@ -56,14 +55,31 @@ export default function About() {
               </div>
             </div>
           </div>
-          {/* <button
-          class="btn btn-primary rounded-0 fw-medium border border-0 mainblue"
-          type="submit"
-        >
-          Register
-        </button> */}
         </div>
-        <div>d</div>
+        <div className="d-flex flex-wrap justify-content-evenly align-items-center">
+          <div className="me-5">
+            <div className="mainblue p-2 d-flex justify-content-center align-items-center rounded-pill my-4">
+              <img src="images/david.png"></img>
+              <div className="pe-3">
+                <p className="fs-5 fw-medium text-light">David Peterson</p>
+                <p className="text-center fs-6 text-light">CEO & Founder</p>
+              </div>
+            </div>
+            <img src="images/discuss.png"></img>
+          </div>
+          <div className="mt-4">
+            <img src="images/worker.png"></img>
+            <div className="mainblue p-2 d-flex justify-content-center align-items-center mt-4">
+              <div className="pe-3 ">
+                <p className="text-center fw-medium text-light mb-2">
+                  Our Team
+                </p>
+                <img src="images/team-circle.png"></img>
+                <p className="text-center text-light mt-2">Web Development</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
