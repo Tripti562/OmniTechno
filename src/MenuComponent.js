8458faff99317f8ddd7037b1255021ef4cf8a0d9
TriptiGautam <gautamtripti1@gmail.com>
2024-05-19 16:49:59 +0545

    used bootstrap

--- src/MenuComponent.js
+++ src/MenuComponent.js
@@ -1,5 +1,13 @@
 // MenuComponent.js
 import React, { useEffect, useRef } from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import About from './About';
+import Career from './Career';
+import Services from './Services';
+import Testimonials from './Testimonials';
+import FooterA from './FooterA';
+import FooterB from './FooterB';
 
 const MenuComponent = () => {
   const menuRef = useRef(null);
@@ -37,16 +45,16 @@ const MenuComponent = () => {
       </div>
       <div id="menu-links" ref={menuLinksRef} className="menu-links p-3">
      
-        <a href="#" onClick={(e) => e.preventDefault()}>
+        <a href="#" >
           <li className="m-1">Home</li>
         </a>
-        <a href="#services" onClick={(e) => e.preventDefault()}>
+        <a href="#services" >
           <li className="m-1">Services</li>
         </a>
-        <a href="#career" onClick={(e) => e.preventDefault()}>
+        <a href="#career" >
           <li className="m-1">Career</li>
         </a>
-        <a href="#contact" onClick={(e) => e.preventDefault()}>
+        <a href="#contact" >
           <li className="m-1">Contact</li>
         </a>
         <button className='mt-2 egister btn btn-primary rounded-0 border border-1' id="btn-close" ref={btnCloseRef}>Close</button>
